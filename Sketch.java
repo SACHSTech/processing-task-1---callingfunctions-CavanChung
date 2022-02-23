8fbd8e257b4270a1fb35594db1806c9a8dbff199
Cavan Chung <75390863+CavanChung@users.noreply.github.com>
2022-02-23 04:49:13 +0000

    Finished the assignment. Drew the house.

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,56 @@
+/**
+ * Name:Processing Task 1 - Calling Functions
+ * Purpose:Drawing House
+ * Author: Cavan Chung
+ * Created:2022/02/22
+ */
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
   public void settings() {
-	// put your size call here
+	// set size
     size(400, 400);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
+  // Set background colour, set/draw background shapes to create shape overlap
   public void setup() {
-    background(210, 255, 173);
+
+    // set background
+    background(173, 216, 230);
+
+    // set/draw grass
+    fill(210, 255, 173);
+    rect(0, 350, 400, 50);
+
+    // set/draw the rectangular house base
+    fill(255, 0, 0);
+    rect(100, 200, 200, 200);  
+
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+	  // draw window 1
+    fill(173,216,230);
+    rect(125, 250, 50, 50);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    // draw window 2
+    fill(173,216,230);
+    rect(225, 250, 50, 50); 
+
+    // draw sun
+    fill(255, 255, 0);
+    ellipse(355, 45, 50, 50);
+
+    // draw house roof
+    fill(165,42,42);
+    triangle(100, 200, 300, 200, 200, 75);
+
+    // draw house door
+    fill(78,53,36);
+    rect(175, 325, 50, 75);
+
   }
   
-  // define other methods down here.
 }
